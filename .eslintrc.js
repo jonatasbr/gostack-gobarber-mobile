d9736399a5af6c76886ff416f9216395d003d0d0
Jonatas Braz <jonatasbraz@gmail.com>
2019-09-09 22:52:35 -0300

    componentes Button e Input

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,12 @@ module.exports = {
     'no-console': ['error', { allow: ['tron'] }],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-props-no-spreading': [
+      {
+        html: 'ignore',
+        custom: 'ignore',
+      },
+    ],
   },
   settings: {
     'import/resolver': {
