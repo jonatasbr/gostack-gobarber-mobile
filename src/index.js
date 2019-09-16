319dfe0196d09e799d0a78c7db2ee5d45fc4ac4b
Jonatas Braz <jonatasbraz@gmail.com>
2019-09-16 09:45:22 -0300

    cfg e chamada de rota dashboard após o login

--- src/index.js
+++ src/index.js
@@ -6,17 +6,15 @@ import { StatusBar } from 'react-native';
 import './config/ReactotronConfig';
 
 import { store, persistor } from './store';
-import Routes from './routes';
+import App from './App';
 
-function App() {
+export default function Index() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <StatusBar barStyle="light-content" backgroundColor="#7459c1" />
-        <Routes />
+        <App />
       </PersistGate>
     </Provider>
   );
 }
-
-export default App;
